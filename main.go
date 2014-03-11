675ba73bd72f6389f8bcec0602b8b4cb5c5577c9
Richard Millan <richardiux@gmail.com>
2014-03-10 17:40:11 -0700

    Handle webp

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"code.google.com/p/graphics-go/graphics"
 	"github.com/gorilla/mux"
+	"github.com/quirkey/magick"
 	"image"
 	"image/jpeg"
 	"io"
@@ -14,8 +15,10 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/product/image/{id:[0-9]+}/{width:[0-9]+}x{height:[0-9]+}.jpg", widthHeightHandler).Methods("GET")
-	r.HandleFunc("/product/image/{id:[0-9]+}/{width:[0-9]+}.jpg", squareHandler).Methods("GET")
+	r.HandleFunc("/product/image/{id:[0-9]+}/{width:[0-9]+}x{height:[0-9]+}.{format}", rectangleHandler).Methods("GET")
+	r.HandleFunc("/product/image/{id:[0-9]+}/x{width:[0-9]+}.{format}", squareHandler).Methods("GET")
+	r.HandleFunc("/product/image/{id:[0-9]+}/w{width:[0-9]+}.{format}", widthHandler).Methods("GET")
+	r.HandleFunc("/product/image/{id:[0-9]+}/full_size.{format}", fullSizeHandler).Methods("GET")
 	http.Handle("/", r)
 	log.Println("Listening on port 3000...")
 	http.ListenAndServe(":3000", nil)
@@ -25,7 +28,7 @@ func downloadAndSaveOriginal(path string, productId string) {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		resp, err := http.Get("http://cdn-s3-2.wanelo.com/product/image/" + productId + "/original.jpg")
 		if err != nil {
-			// panic?
+			log.Fatal(err)
 		}
 		defer resp.Body.Close()
 		out, err := os.Create(path)
@@ -36,7 +39,7 @@ func downloadAndSaveOriginal(path string, productId string) {
 	}
 }
 
-func createResizedImage(fullSizePath string, resizedPath string, width string, height string) {
+func createJPG(fullSizePath string, resizedPath string, width string, height string) {
 	file, err := os.Open(fullSizePath)
 	if err != nil {
 		log.Fatal(err)
@@ -66,22 +69,55 @@ func createResizedImage(fullSizePath string, resizedPath string, width string, h
 	}
 }
 
-func createImages(id string, width string, height string) (path string) {
+func createWEBP(fullSizePath string, resizedPath string, width string, height string) {
+	image, err := magick.NewFromFile(fullSizePath)
+
+	err = image.Resize(height + "x" + width + "^")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = image.SetProperty("gravity", "center")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = image.SetProperty("extent", height+"x"+width)
+	if err != nil {
+		log.Fatal(err)
+	}
+	/*	image.Quality(85)*/
+	/*	err = image.Crop(width + "x" + height + "")*/
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = image.ToFile(resizedPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createImages(id string, width string, height string, format string) (path string) {
 	fullSizePath := "public/" + id
-	resizedPath := "public/" + id + "_" + width + "x" + height + ".jpg"
+	resizedPath := "public/" + id + "_" + width + "x" + height + "." + format
 
 	downloadAndSaveOriginal(fullSizePath, id)
-	createResizedImage(fullSizePath, resizedPath, width, height)
+	if format == "jpg" {
+		createJPG(fullSizePath, resizedPath, width, height)
+	} else if format == "webp" {
+		createWEBP(fullSizePath, resizedPath, width, height)
+	}
+
 	return resizedPath
 }
 
-func widthHeightHandler(w http.ResponseWriter, r *http.Request) {
+func rectangleHandler(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	id := params["id"]
 	width := params["width"]
 	height := params["height"]
+	format := params["format"]
 
-	resizedPath := createImages(id, width, height)
+	resizedPath := createImages(id, width, height, format)
 	http.ServeFile(w, r, resizedPath)
 }
 
@@ -90,7 +126,55 @@ func squareHandler(w http.ResponseWriter, r *http.Request) {
 	id := params["id"]
 	width := params["width"]
 	height := params["width"]
+	format := params["format"]
 
-	resizedPath := createImages(id, width, height)
+	resizedPath := createImages(id, width, height, format)
 	http.ServeFile(w, r, resizedPath)
+}
+
+func widthHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id := params["id"]
+	width := params["width"]
+	format := params["format"]
+
+	fullSizePath := "public/" + id
+	resizedPath := "public/" + id + "_w" + width + "." + format
+
+	downloadAndSaveOriginal(fullSizePath, id)
+
+	image, err := magick.NewFromFile(fullSizePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = image.Resize(width)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = image.ToFile(resizedPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.ServeFile(w, r, resizedPath)
+}
+
+func fullSizeHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id := params["id"]
+	format := params["format"]
+
+	fullSizePath := "public/" + id
+	resizedPath := "public/" + id + "_full_size." + format
+
+	downloadAndSaveOriginal(fullSizePath, id)
+
+	image, err := magick.NewFromFile(fullSizePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = image.ToFile(resizedPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.ServeFile(w, r, fullSizePath)
 }
