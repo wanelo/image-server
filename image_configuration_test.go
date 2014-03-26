3a5f17c5507200965a17d2e6978e45f928e91de0
Richard Millan <richardiux@gmail.com>
2014-03-25 22:10:13 -0700

    First test!!

---
+++ image_configuration_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func TestRemoteImageUrl(t *testing.T) {
+	serverConfiguration, _ = NewServerConfiguration("config/test.json")
+	ic := &ImageConfiguration{model: "product", imageType: "image", id: "55"}
+	url := ic.RemoteImageUrl()
+	expectedUrl := "http://cdn-s3-2.wanelo.com/product/image/55/original.jpg"
+
+	if url != expectedUrl {
+		t.Errorf("expected %s to be %s", url, expectedUrl)
+	}
+}
