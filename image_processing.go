a12066eaf9386556bc7d311d497a9df114ccec76
Richard Millan <richardiux@gmail.com>
2014-03-27 22:08:24 -0700

    Split image fetching

--- image_processing.go
+++ image_processing.go
@@ -1,53 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/rainycape/magick"
 )
-
-func downloadAndSaveOriginal(ic *ImageConfiguration) error {
-	path := ic.LocalOriginalImagePath()
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		start := time.Now()
-
-		remoteUrl := ic.RemoteImageUrl()
-		resp, err := http.Get(remoteUrl)
-
-		log.Printf("response code %d", resp.StatusCode)
-		if err != nil || resp.StatusCode != 200 {
-			log.Printf("Unable to download image: %s, status code: %d", remoteUrl, resp.StatusCode)
-			log.Println(err)
-			return fmt.Errorf("Unable to download image: %s, status code: %d", remoteUrl, resp.StatusCode)
-		}
-		defer resp.Body.Close()
-
-		dir := filepath.Dir(path)
-		os.MkdirAll(dir, 0700)
-
-		out, err := os.Create(path)
-		defer out.Close()
-		if err != nil {
-			log.Printf("Unable to create file: %s", path)
-			log.Println(err)
-			return fmt.Errorf("Unable to create file: %s", path)
-		}
-
-		io.Copy(out, resp.Body)
-		log.Printf("Took %s to download image: %s", time.Since(start), path)
-
-		go func() {
-			sendToManta(path, ic.MantaOriginalImagePath())
-		}()
-	}
-	return nil
-}
 
 func createWithMagick(ic *ImageConfiguration) {
 	start := time.Now()
