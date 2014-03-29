9858a7219ccb8d31eb5aad08ab1d4827d5c739d0
Richard Millan <richardiux@gmail.com>
2014-03-29 10:12:27 -0700

    NameToConfiguration tests format extraction

--- name_parser_test.go
+++ name_parser_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func ensureDimensions(t *testing.T, ic *ImageConfiguration, w int, h int) {
+func ensureDimensions(t *testing.T, ic *ImageConfiguration, w int, h int, f string) {
 	if ic.width != w {
 		t.Errorf("expected %v to be %v", ic.width, w)
 
@@ -10,26 +10,29 @@ func ensureDimensions(t *testing.T, ic *ImageConfiguration, w int, h int) {
 	if ic.height != h {
 		t.Errorf("expected %v to be %v", ic.width, h)
 	}
+	if ic.format != f {
+		t.Errorf("expected %v to be %v", ic.format, f)
+	}
 }
 
 func TestRectangle(t *testing.T) {
 	ic, _ := NameToConfiguration("300x400.jpg")
-	ensureDimensions(t, ic, 300, 400)
+	ensureDimensions(t, ic, 300, 400, "jpg")
 }
 
 func TestSquare(t *testing.T) {
 	ic, _ := NameToConfiguration("x300.jpg")
-	ensureDimensions(t, ic, 300, 300)
+	ensureDimensions(t, ic, 300, 300, "jpg")
 }
 
 func TestWidth(t *testing.T) {
 	ic, _ := NameToConfiguration("w300.jpg")
-	ensureDimensions(t, ic, 300, 0)
+	ensureDimensions(t, ic, 300, 0, "jpg")
 }
 
 func TestFullSize(t *testing.T) {
 	ic, _ := NameToConfiguration("full_size.jpg")
-	ensureDimensions(t, ic, 0, 0)
+	ensureDimensions(t, ic, 0, 0, "jpg")
 }
 
 func TestUnsupported(t *testing.T) {
