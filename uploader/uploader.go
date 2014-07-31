cf8332da76698e1d08d910af16a8192e9353d022
Richard Millan <richard@wanelo.com>
2014-07-30 22:42:05 -0700

    Log uploader duration time

--- uploader/uploader.go
+++ uploader/uploader.go
@@ -1,20 +1,32 @@
 package uploader
 
-import "github.com/wanelo/image-server/uploader/manta"
+import (
+	"log"
+	"time"
+
+	"github.com/wanelo/image-server/uploader/manta"
+)
 
 type Uploader struct {
 	BaseDir string
 }
 
 func (u *Uploader) Upload(source string, destination string) error {
+	start := time.Now()
 	uploader := manta.DefaultUploader()
 	err := uploader.Upload(source, destination)
+	elapsed := time.Since(start)
+	log.Printf("Took %s to upload image: %s", elapsed, destination)
 	return err
 }
 
 func (u *Uploader) CreateDirectory(path string) error {
+	start := time.Now()
 	uploader := manta.DefaultUploader()
-	return uploader.CreateDirectory(path)
+	elapsed := time.Since(start)
+	directoryPath := uploader.CreateDirectory(path)
+	log.Printf("Took %s to generate remote directory: %s", elapsed, path)
+	return directoryPath
 }
 
 func (u *Uploader) Initialize() error {
