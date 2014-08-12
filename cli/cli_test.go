5928c5f729098ecf1c75563cf9558cd34950f338
Richard Millan <richard@wanelo.com>
2014-08-11 22:40:22 -0700

    Test helpers are included directly on package

--- cli/cli_test.go
+++ cli/cli_test.go
@@ -3,18 +3,18 @@ package cli
 import (
 	"testing"
 
-	"github.com/wanelo/image-server/test"
+	. "github.com/wanelo/image-server/test"
 )
 
 func TestLineToItem(t *testing.T) {
 	item, err := lineToItem("6ad5544baa6f5e852e1af26f8c2e45db http://example.com/image.url")
-	test.Ok(t, err)
-	test.Equals(t, "6ad5544baa6f5e852e1af26f8c2e45db", item.Hash)
-	test.Equals(t, "http://example.com/image.url", item.URL)
+	Ok(t, err)
+	Equals(t, "6ad5544baa6f5e852e1af26f8c2e45db", item.Hash)
+	Equals(t, "http://example.com/image.url", item.URL)
 }
 
 func TestItemToTabDelimited(t *testing.T) {
 	item := Item{"6ad5544baa6f5e852e1af26f8c2e45db", "http://example.com/image.jpg", 40, 30}
 	expected := "6ad5544baa6f5e852e1af26f8c2e45db\thttp://example.com/image.jpg\t40\t30"
-	test.Equals(t, expected, item.ToTabDelimited())
+	Equals(t, expected, item.ToTabDelimited())
 }
