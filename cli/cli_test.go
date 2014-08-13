dd8db8c6f317037af6329e11126a82e42d67b0ab
Jo Pu & Richard Millan <jo+richard@wanelo.com>
2014-08-12 18:38:47 -0700

    Fix failing test

--- cli/cli_test.go
+++ cli/cli_test.go
@@ -15,6 +15,6 @@ func TestLineToItem(t *testing.T) {
 
 func TestItemToTabDelimited(t *testing.T) {
 	item := Item{"6ad5544baa6f5e852e1af26f8c2e45db", "http://example.com/image.jpg", 40, 30}
-	expected := "6ad5544baa6f5e852e1af26f8c2e45db\thttp://example.com/image.jpg\t40\t30"
+	expected := "6ad5544baa6f5e852e1af26f8c2e45db\thttp://example.com/image.jpg\t40\t30\n"
 	Equals(t, expected, item.ToTabDelimited())
 }
