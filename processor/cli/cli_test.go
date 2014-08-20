f7abe01d42f97ed559d0d7ec038e60872e88bd2d
Andre Arko & Richard Millan <andre+richard@wanelo.com>
2014-08-19 17:44:15 -0700

    remove backslashes that break converting images

--- processor/cli/cli_test.go
+++ processor/cli/cli_test.go
@@ -10,8 +10,8 @@ import (
 func TestFullSizeImage(t *testing.T) {
 	ic := &core.ImageConfiguration{Width: 0, Height: 0, Format: "jpg", Quality: 85, Namespace: "test", ID: "ofrA", Filename: "full_size.jpg"}
 
-	expected := []string{"-format", "jpg", "-flatten", "-background", "rgba\\(255,255,255,1\\)", "-quality", "85", "public/test/00/of/rA/original", "public/test/00/of/rA/full_size.jpg"}
-  p := Processor{}
+	expected := []string{"-format", "jpg", "-flatten", "-background", "rgba(255,255,255,1)", "-quality", "85", "public/test/00/of/rA/original", "public/test/00/of/rA/full_size.jpg"}
+	p := Processor{}
 	command := p.commandArgs("public/test/00/of/rA/original", "public/test/00/of/rA/full_size.jpg", ic)
 	if !reflect.DeepEqual(expected, command) {
 		t.Errorf("expected\n%v got\n%v", expected, command)
@@ -21,7 +21,7 @@ func TestFullSizeImage(t *testing.T) {
 func TestImageWithWidth(t *testing.T) {
 	ic := &core.ImageConfiguration{Width: 600, Height: 0, Format: "jpg", Quality: 85, Namespace: "test", ID: "ofrA", Filename: "w600.jpg"}
 
-	expected := []string{"-format", "jpg", "-flatten", "-resize", "600", "-background", "rgba\\(255,255,255,1\\)", "-quality", "85", "public/test/00/of/rA/original", "public/test/00/of/rA/w600.jpg"}
+	expected := []string{"-format", "jpg", "-flatten", "-resize", "600", "-background", "rgba(255,255,255,1)", "-quality", "85", "public/test/00/of/rA/original", "public/test/00/of/rA/w600.jpg"}
 
 	p := Processor{}
 	command := p.commandArgs("public/test/00/of/rA/original", "public/test/00/of/rA/w600.jpg", ic)
@@ -33,7 +33,7 @@ func TestImageWithWidth(t *testing.T) {
 func TestImageWithWidthAndHeight(t *testing.T) {
 	ic := &core.ImageConfiguration{Width: 600, Height: 500, Format: "jpg", Quality: 85, Namespace: "test", ID: "ofrA", Filename: "600x500.jpg"}
 
-	expected := []string{"-format", "jpg", "-flatten", "-extent", "600x500", "-gravity", "center", "-background", "rgba\\(255,255,255,1\\)", "-quality", "85", "public/test/00/of/rA/original", "public/test/00/of/rA/600x500.jpg"}
+	expected := []string{"-format", "jpg", "-flatten", "-extent", "600x500", "-gravity", "center", "-background", "rgba(255,255,255,1)", "-quality", "85", "public/test/00/of/rA/original", "public/test/00/of/rA/600x500.jpg"}
 
 	p := Processor{}
 	command := p.commandArgs("public/test/00/of/rA/original", "public/test/00/of/rA/600x500.jpg", ic)
