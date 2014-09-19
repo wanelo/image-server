13a2050a4ea73b5377733139fc108d74b4b3a8b4
Richard Millan <richard@wanelo.com>
2014-09-19 15:15:28 -0700

    Image configuration is able to determine content type based on extension of image

--- core/image_configuration.go
+++ core/image_configuration.go
@@ -1,5 +1,10 @@
 package core
 
+import (
+	"fmt"
+	"mime"
+)
+
 // ImageConfiguration struct
 // Properties used to generate new image
 type ImageConfiguration struct {
@@ -14,35 +19,8 @@ type ImageConfiguration struct {
 	Namespace string
 }
 
-// func (ic *ImageConfiguration) ImageDirectory() string {
-// 	// ic.ServerConfiguration.Adapters.Paths
-// 	id := strings.Join(ic.IDPartitions(), "/")
-//
-// 	return fmt.Sprintf("%s/%s", ic.Namespace, id)
-// }
-
-// func (ic *ImageConfiguration) IDPartitions() []string {
-// 	digits := fmt.Sprintf("%06s", ic.ID)
-//
-// 	return []string{digits[0:2], digits[2:4], digits[4:6]}
-// }
-//
-// func (ic *ImageConfiguration) LocalDestinationDirectory() string {
-// 	return ic.ServerConfiguration.LocalBasePath + "/" + ic.ImageDirectory()
-// }
-//
-// func (ic *ImageConfiguration) LocalOriginalImagePath() string {
-// 	return ic.LocalDestinationDirectory() + "/original"
-// }
-//
-// func (ic *ImageConfiguration) LocalResizedImagePath() string {
-// 	return ic.LocalDestinationDirectory() + "/" + ic.Filename
-// }
-//
-// func (sc *ServerConfiguration) MantaResizedImagePath(ic *ImageConfiguration) string {
-// 	return sc.RemoteBasePath + "/" + ic.ImageDirectory() + "/" + ic.Filename
-// }
-//
-// func (sc *ServerConfiguration) MantaOriginalImagePath(ic *ImageConfiguration) string {
-// 	return sc.RemoteBasePath + "/" + ic.ImageDirectory() + "/original"
-// }
+// ToContentType returns the content type based on the image format
+func (ic *ImageConfiguration) ToContentType() string {
+	ext := fmt.Sprintf(".%s", ic.Format)
+	return mime.TypeByExtension(ext)
+}
--- core/image_configuration_test.go
+++ core/image_configuration_test.go
@@ -1,1 +1,32 @@
-package core
+package core_test
+
+import (
+	"testing"
+
+	"github.com/wanelo/image-server/core"
+	. "github.com/wanelo/image-server/test"
+)
+
+func TestToContentTypeForJPEG(t *testing.T) {
+	ic := &core.ImageConfiguration{Format: "jpeg"}
+
+	Equals(t, "image/jpeg", ic.ToContentType())
+}
+
+func TestToContentTypeForJpg(t *testing.T) {
+	ic := &core.ImageConfiguration{Format: "jpg"}
+
+	Equals(t, "image/jpeg", ic.ToContentType())
+}
+
+func TestToContentTypeForWebp(t *testing.T) {
+	ic := &core.ImageConfiguration{Format: "webp"}
+
+	Equals(t, "image/webp", ic.ToContentType())
+}
+
+func TestToContentTypeForGIF(t *testing.T) {
+	ic := &core.ImageConfiguration{Format: "gif"}
+
+	Equals(t, "image/gif", ic.ToContentType())
+}
