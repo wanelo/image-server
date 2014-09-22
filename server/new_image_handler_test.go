ff4bc1ea85adcd6352cad427b29db2730e87d4c1
Eric Saxby & Richard Millan <eric+richard@wanelo.com>
2014-09-22 15:31:37 -0700

    Test S3 upload, allow _ in namespace
    - Pull out server.NewRouter() to make testing easier

---
+++ server/new_image_handler_test.go
@@ -0,0 +1,89 @@
+package server_test
+
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/mitchellh/goamz/aws"
+	"github.com/mitchellh/goamz/s3"
+	"github.com/wanelo/image-server/core"
+	fetcher "github.com/wanelo/image-server/fetcher/http"
+	"github.com/wanelo/image-server/logger"
+	"github.com/wanelo/image-server/logger/graphite"
+	"github.com/wanelo/image-server/paths"
+	processor "github.com/wanelo/image-server/processor/cli"
+	"github.com/wanelo/image-server/server"
+
+	. "github.com/wanelo/image-server/test"
+)
+
+func TestNewImageHandlerWithS3(t *testing.T) {
+	deleteS3TestDirectory()
+
+	sc := buildTestServerConfiguration()
+	router := server.NewRouter(sc)
+
+	request, _ := http.NewRequest("POST", "/test_namespace?outputs=x300.jpg,x300.webp&source=http%3A%2F%2Fcdn-s3-3.wanelo.com%2Fproduct%2Fimage%2F15209365%2Fx354.jpg", nil)
+	response := httptest.NewRecorder()
+	log.Println(sc)
+
+	router.ServeHTTP(response, request)
+
+	url := "https://s3.amazonaws.com/wanelo-dev/test/test_namespace/6da/b5f/6d8/d4bddc73fdff34d4f0507f7/info.json"
+	resp, err := http.Head(url)
+	Ok(t, err)
+	Equals(t, "200 OK", resp.Status)
+	Equals(t, "application/json", resp.Header.Get("Content-Type"))
+
+	url = "https://s3.amazonaws.com/wanelo-dev/test/test_namespace/6da/b5f/6d8/d4bddc73fdff34d4f0507f7/x300.jpg"
+	resp, err = http.Head(url)
+	Ok(t, err)
+	Equals(t, "200 OK", resp.Status)
+	Equals(t, "image/jpeg", resp.Header.Get("Content-Type"))
+
+	url = "https://s3.amazonaws.com/wanelo-dev/test/test_namespace/6da/b5f/6d8/d4bddc73fdff34d4f0507f7/x300.webp"
+	resp, err = http.Head(url)
+	Ok(t, err)
+	Equals(t, "200 OK", resp.Status)
+	Equals(t, "image/webp", resp.Header.Get("Content-Type"))
+}
+
+func deleteS3TestDirectory() {
+	auth := aws.Auth{
+		AccessKey: os.Getenv("AWS_ACCESS_KEY_ID"),
+		SecretKey: os.Getenv("AWS_SECRET_KEY"),
+	}
+	client := s3.New(auth, aws.USEast)
+	bucket := client.Bucket(os.Getenv("AWS_BUCKET"))
+	bucket.Del("/test/test_namespace/6da/b5f/6d8/d4bddc73fdff34d4f0507f7/info.json")
+	bucket.Del("/test/test_namespace/6da/b5f/6d8/d4bddc73fdff34d4f0507f7/original")
+	bucket.Del("/test/test_namespace/6da/b5f/6d8/d4bddc73fdff34d4f0507f7/x300.jpg")
+	bucket.Del("/test/test_namespace/6da/b5f/6d8/d4bddc73fdff34d4f0507f7/x300.webp")
+}
+
+func buildTestServerConfiguration() *core.ServerConfiguration {
+	sc := &core.ServerConfiguration{
+		LocalBasePath:  "../public",
+		RemoteBasePath: "test",
+		DefaultQuality: 90,
+		AWSAccessKeyID: os.Getenv("AWS_ACCESS_KEY_ID"),
+		AWSSecretKey:   os.Getenv("AWS_SECRET_KEY"),
+		AWSBucket:      os.Getenv("AWS_BUCKET"),
+	}
+
+	loggers := []core.Logger{
+		graphite.New(sc.GraphiteHost, sc.GraphitePort),
+	}
+
+	adapters := &core.Adapters{
+		Fetcher:   &fetcher.Fetcher{},
+		Processor: &processor.Processor{},
+		Paths:     &paths.Paths{LocalBasePath: sc.LocalBasePath, RemoteBasePath: sc.RemoteBasePath, RemoteBaseURL: sc.RemoteBaseURL},
+		Logger:    &logger.Logger{Loggers: loggers},
+	}
+	sc.Adapters = adapters
+	return sc
+}
--- server/server.go
+++ server/server.go
@@ -12,27 +12,33 @@ import (
 func InitializeRouter(sc *core.ServerConfiguration, listen string, port string) {
 	log.Printf("starting server on http://%s:%s", listen, port)
 
-	router := mux.NewRouter()
-	router.HandleFunc("/{namespace:[a-z0-9]+}", func(wr http.ResponseWriter, req *http.Request) {
-		NewImageHandler(wr, req, sc)
-	}).Methods("POST").Name("newImage")
-
-	router.HandleFunc("/{namespace:[a-z0-9]+}/{id1:[a-f0-9]{3}}/{id2:[a-f0-9]{3}}/{id3:[a-f0-9]{3}}/{id4:[a-f0-9]{23}}/{filename}", func(wr http.ResponseWriter, req *http.Request) {
-		ResizeHandler(wr, req, sc)
-	}).Methods("GET").Name("resizeImage")
-
-	router.HandleFunc("/{namespace}/batch", func(wr http.ResponseWriter, req *http.Request) {
-		CreateBatchHandler(wr, req, sc)
-	}).Methods("POST").Name("createBatch")
-
-	router.HandleFunc("/{namespace}/batch/{uuid:[a-f0-9-]{36}}", func(wr http.ResponseWriter, req *http.Request) {
-		BatchHandler(wr, req, sc)
-	}).Methods("GET").Name("batch")
-
-	router.HandleFunc("/status_check", StatusHandler)
+	router := NewRouter(sc)
 
 	n := negroni.Classic()
 	n.UseHandler(router)
 
 	n.Run(listen + ":" + port)
+}
+
+// NewRouter creates a mux.Router for use in code or in tests
+func NewRouter(sc *core.ServerConfiguration) *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/{namespace:[a-z0-9_]+}", func(wr http.ResponseWriter, req *http.Request) {
+		NewImageHandler(wr, req, sc)
+	}).Methods("POST").Name("newImage")
+
+	router.HandleFunc("/{namespace:[a-z0-9_]+}/{id1:[a-f0-9]{3}}/{id2:[a-f0-9]{3}}/{id3:[a-f0-9]{3}}/{id4:[a-f0-9]{23}}/{filename}", func(wr http.ResponseWriter, req *http.Request) {
+		ResizeHandler(wr, req, sc)
+	}).Methods("GET").Name("resizeImage")
+
+	router.HandleFunc("/{namespace:[a-z0-9_]+}/batch", func(wr http.ResponseWriter, req *http.Request) {
+		CreateBatchHandler(wr, req, sc)
+	}).Methods("POST").Name("createBatch")
+
+	router.HandleFunc("/{namespace:[a-z0-9_]+}/batch/{uuid:[a-f0-9-]{36}}", func(wr http.ResponseWriter, req *http.Request) {
+		BatchHandler(wr, req, sc)
+	}).Methods("GET").Name("batch")
+
+	router.HandleFunc("/status_check", StatusHandler)
+	return router
 }
