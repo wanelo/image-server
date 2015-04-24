7368603c8c94b996269092f47a37877fffe09d17
Richard Millan <richardiux@gmail.com>
2015-04-23 23:57:58 -0700

    Fixes formatting on command usage

--- cmd_server.go
+++ cmd_server.go
@@ -15,47 +15,45 @@ var cmdServer = &Command{
 	UsageLine: "server [flags]",
 	Short:     "image server",
 	Long: `
+Flags:
+    SERVER
+    --listen '127.0.0.1'        IP address the server listens to
+    --port '7000'               Specifies the server port.
+    --local_base_path 'public'  Directory where the images will be saved
+    --outputs                   i.e. 'full_size.jpg,full_size.webp,x110-q90.jpg,x200-q90.jpg'
 
-	Flags:
-	 SERVER
-	   --listen '127.0.0.1'									IP address the server listens to
-	   --port '7000'												Specifies the server port.
-   	 --local_base_path 'public'						Directory where the images will be saved
-     --outputs                            i.e. 'full_size.jpg,full_size.webp,x110-q90.jpg,x200-q90.jpg'
+  UPLOADERS:
+  Images can be uploaded to Amazon S3 or Joyent's Manta if the following flags are provided.
+    --remote_base_url   Source domain for images
+    --remote_base_path  Base path for cloud storage
 
-	 UPLOADERS:
-	 Images can be uploaded to Amazon S3 or Joyent's Manta if the following flags are provided.
-		 --remote_base_url 			Source domain for images
-		 --remote_base_path 		base path for cloud storage
+    S3
+    --aws_access_key_id
+    --aws_secret_key
+    --aws_bucket
 
-		 S3
-		 --aws_access_key_id
-	   --aws_secret_key
-	   --aws_bucket
+    MANTA
+    --manta_url     URL of API. https://us-east.manta.joyent.com
+    --manta_user    The account name
+    --manta_key_id  The fingerprint of the account or user SSH public key. Example: $(ssh-keygen -l -f $HOME/.ssh/id_rsa.pub | awk '{print $2}')
+    --sdc_identity  Example: $HOME/.ssh/id_rsa
 
-		 MANTA
-	   --manta_url 			URL of API. https://us-east.manta.joyent.com
-	   --manta_user 		The account name
-	   --manta_key_id 	The fingerprint of the account or user SSH public key. Example: $(ssh-keygen -l -f $HOME/.ssh/id_rsa.pub | awk '{print $2}')
-	   --sdc_identity 	Example: $HOME/.ssh/id_rsa
+  IMAGE CONFIGURATIONS
+    --namespace                 Default namespace
+    --extensions 'jpg,gif,webp' Whitelisted extensions (separated by commas)
+    --maximum_width '1000'      Maximum image width
+    --default_quality '75'      Default image compression quality
 
-	 IMAGE CONFIGURATIONS
-		 --namespace 									Default namespace
-	   --extensions 'jpg,gif,webp'	Whitelisted extensions (separated by commas)
-	   --maximum_width '1000'				Maximum image width
-	   --default_quality '75'       Default image compression quality
+  SERVER TUNNING
+    --uploader_concurrency '10' Uploader concurrency
+    --processor_concurrency '4' Processor concurrency
+    --http_timeout '5'          HTTP request timeout in seconds
+    --gomaxprocs '0'            It will use the default when set to 0
 
-	 SERVER TUNNING
-	   --uploader_concurrency '10'					Uploader concurrency
-	   --processor_concurrency '4'					Processor concurrency
-	   --http_timeout '5'										HTTP request timeout in seconds
-	   --gomaxprocs '0'											It will use the default when set to 0
-
-	 MONITORING & PROFILING
-		 --profile										Enable pprof
-		 --graphite_host '127.0.0.1'	Graphite host
-		 --graphite_port '8125'				Graphite port
-	`,
+  MONITORING & PROFILING
+    --profile                   Enable pprof
+    --graphite_host '127.0.0.1' Graphite host
+    --graphite_port '8125'      Graphite port	`,
 }
 
 func init() {
