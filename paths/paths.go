7903f25319e84832c7a29672322469900d90fee8
Richard Millan <richardiux@gmail.com>
2016-04-22 16:19:12 -0700

    Construct paths using `filepath.Join` instead of `fmt.Sprintf`

--- paths/paths.go
+++ paths/paths.go
@@ -5,6 +5,8 @@ import (
 	"crypto/rand"
 	"fmt"
 	"strings"
+	"path/filepath"
+	"net/url"
 )
 
 // Paths
@@ -16,74 +18,76 @@ type Paths struct {
 
 // LocalOriginalPath returns local path for original image
 func (p *Paths) LocalOriginalPath(namespace string, md5 string) string {
-	return fmt.Sprintf("%s/%s", p.LocalBasePath, p.originalPath(namespace, md5))
+	return filepath.Join(p.LocalBasePath, p.originalPath(namespace, md5))
 }
 
 // LocalImageDirectory returns location for locally cached images
 func (p *Paths) LocalImageDirectory(namespace string, md5 string) string {
-	return fmt.Sprintf("%s/%s", p.LocalBasePath, p.imageDirectory(namespace, md5))
+	return filepath.Join(p.LocalBasePath, p.imageDirectory(namespace, md5))
 }
 
 // RemoteImageDirectory returns location for directory for images and info
 func (p *Paths) RemoteImageDirectory(namespace string, md5 string) string {
-	return fmt.Sprintf("%s/%s", p.RemoteBasePath, p.imageDirectory(namespace, md5))
+	return filepath.Join(p.RemoteBasePath, p.imageDirectory(namespace, md5))
 }
 
 //  LocalImagePath returns local path for resized image
 func (p *Paths) LocalImagePath(namespace string, md5 string, imageName string) string {
-	return fmt.Sprintf("%s/%s", p.LocalBasePath, p.imagePath(namespace, md5, imageName))
+	return filepath.Join(p.LocalBasePath, p.imagePath(namespace, md5, imageName))
 }
 
 func (p *Paths) RemoteImagePath(namespace string, md5 string, imageName string) string {
-	return fmt.Sprintf("%s/%s", p.RemoteBasePath, p.imagePath(namespace, md5, imageName))
+	return filepath.Join(p.RemoteBasePath, p.imagePath(namespace, md5, imageName))
 }
 
 // RemoteOriginalPath returns local path for original image
 func (p *Paths) RemoteOriginalPath(namespace string, md5 string) string {
-	return fmt.Sprintf("%s/%s", p.RemoteBasePath, p.originalPath(namespace, md5))
+	return filepath.Join(p.RemoteBasePath, p.originalPath(namespace, md5))
 }
 
 func (p *Paths) RemoteOriginalURL(namespace string, md5 string) string {
-	return fmt.Sprintf("%s/%s", p.RemoteBaseURL, p.RemoteOriginalPath(namespace, md5))
+	u, _ := url.Parse(p.RemoteBaseURL)
+	u.Path = filepath.Join(u.Path, p.RemoteOriginalPath(namespace, md5))
+	return u.String()
 }
 
 func (p *Paths) LocalInfoPath(namespace string, md5 string) string {
-	return fmt.Sprintf("%s/%s", p.LocalBasePath, p.infoPath(namespace, md5))
+	return filepath.Join(p.LocalBasePath, p.infoPath(namespace, md5))
 }
 
 func (p *Paths) RemoteInfoPath(namespace string, md5 string) string {
-	return fmt.Sprintf("%s/%s", p.RemoteBasePath, p.infoPath(namespace, md5))
+	return filepath.Join(p.RemoteBasePath, p.infoPath(namespace, md5))
 }
 
 func (p *Paths) TempImagePath(url string) string {
 	data := []byte(url)
 	name := fmt.Sprintf("%x", md5.Sum(data))
-	return fmt.Sprintf("%s/tmp/%s", p.LocalBasePath, name)
+	return filepath.Join(p.LocalBasePath, "tmp", name)
 }
 
 func (p *Paths) RandomTempPath() string {
 	b := make([]byte, 16)
 	rand.Read(b)
 	name := fmt.Sprintf("%x", b)
-	return fmt.Sprintf("%s/tmp/%s", p.LocalBasePath, name)
+	return filepath.Join(p.LocalBasePath, "tmp", name)
 }
 
 // originalPath
 func (p *Paths) originalPath(namespace string, md5 string) string {
-	return fmt.Sprintf("%s/original", p.imageDirectory(namespace, md5))
+	return filepath.Join(p.imageDirectory(namespace, md5), "original")
 }
 
 // imageDirectory returns relative directory starting at image root
 func (p *Paths) imageDirectory(namespace string, md5 string) string {
 	partitions := []string{md5[0:3], md5[3:6], md5[6:9], md5[9:32]}
-	return fmt.Sprintf("%s/%s", namespace, strings.Join(partitions, "/"))
+	return filepath.Join(namespace, strings.Join(partitions, "/"))
 }
 
 func (p *Paths) infoPath(namespace string, md5 string) string {
-	return fmt.Sprintf("%s/info.json", p.imageDirectory(namespace, md5))
+	return filepath.Join(p.imageDirectory(namespace, md5), "info.json")
 }
 
 //  imagePath returns relative path to resized image
 func (p *Paths) imagePath(namespace string, md5 string, imageName string) string {
-	return fmt.Sprintf("%s/%s", p.imageDirectory(namespace, md5), imageName)
+	return filepath.Join(p.imageDirectory(namespace, md5), imageName)
 }
