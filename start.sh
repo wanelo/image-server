51f550aae7983496f4fac4f12402b68ab47850d2
Thomas Barton <thomasryanbarton@gmail.com>
2017-09-15 13:08:13 -0700

    Fix docker graceful stop

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ AWS_REGION=${AWS_REGION:-us-east-1}
 SERVER_LISTEN=${SERVER_LISTEN:-0.0.0.0}
 IMAGE_SERVER_REMOTE_BASE_URL=${IMAGE_SERVER_REMOTE_BASE_URL:-https://s3-${AWS_REGION}.amazonaws.com/${AWS_S3_BUCKET}}
 
-bin/image-server --local_base_path ${BASE_PATH} --uploader ${UPLOADER} --aws_bucket ${AWS_S3_BUCKET} --aws_region ${AWS_REGION} --listen ${SERVER_LISTEN} --remote_base_url ${IMAGE_SERVER_REMOTE_BASE_URL} server
+exec bin/image-server --local_base_path ${BASE_PATH} --uploader ${UPLOADER} --aws_bucket ${AWS_S3_BUCKET} --aws_region ${AWS_REGION} --listen ${SERVER_LISTEN} --remote_base_url ${IMAGE_SERVER_REMOTE_BASE_URL} server
