f765f66b31cd352a046b2a58d5052e23ab023a0c
Thomas Barton <thomasryanbarton@gmail.com>
2018-06-25 22:50:47 -0700

    Add garbage collector tickID

--- file_garbage_collector/file_garbage_collector.go
+++ file_garbage_collector/file_garbage_collector.go
@@ -26,19 +26,22 @@ func Start(sc *core.ServerConfiguration) {
 					return nil
 				})
 				for range sc.CleanUpTicker.C {
+					tickTime := time.Now()
+					log.Printf("Starting [tickID: %v]\n", tickTime)
 					filepath.Walk(absolutePath, func(path string, info os.FileInfo, err error) error {
 						if !info.IsDir() {
-							age := time.Now().Sub(info.ModTime())
+							age := tickTime.Sub(info.ModTime())
 							if age > sc.MaxFileAge {
-								log.Printf("Deleting file [%s] size [%d] modTime [%s] age [%s]\n", path, info.Size(), info.ModTime(), age)
+								log.Printf("[tickID: %v] Deleting file [%s] size [%d] modTime [%s] age [%s]\n", tickTime, path, info.Size(), info.ModTime(), age)
 								var err = os.Remove(path)
 								if err != nil {
-									log.Printf("Error deleting file [%s]\n", path)
+									log.Printf("[tickID: %v] Error deleting file [%s]\n", tickTime, path)
 								}
 							}
 						}
 						return nil
 					})
+					log.Printf("Finished [tickID: %v]\n", tickTime)
 				}
 			} else {
 				log.Printf("Error Starting File Cleaner - Invalid walk path [%s]", absolutePath)
